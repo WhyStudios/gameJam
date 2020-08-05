158bd06337b9ecc178b8e40f9ad6f2d49b5ca934
LucaBarzan <lucafxx360@hotmail.com>
2020-08-05 12:03:31 -0300

    -Object pooling falhou; -Texturas otimizadas;

--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] tiles;
     private MenuManager menuManager;
     private Player player;
     private float timeToAddDistance;
@@ -33,6 +34,23 @@ public class GameManager : MonoBehaviour
 
             menuManager.SetDistance(distance);
         }
+    }
+
+    public void ActivateRandomTile(Vector3 transform)
+    {
+        List<GameObject> tilesUsable = new List<GameObject>();
+        tilesUsable.Clear();
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            if (!tiles[i].activeSelf) tilesUsable.Add(tiles[i]);
+        }
+
+        int tileNumber = Random.Range(0, tilesUsable.Count);
+        GameObject tile = tilesUsable[tileNumber];
+        tile.SetActive(true);
+        tile.transform.position = transform;
+        Debug.Log(tile.name);
     }
 
     public static void StopGameplay()
--- GameJam/Assets/Scripts/Piece.cs
+++ GameJam/Assets/Scripts/Piece.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    private GameObject[] tiles;
+    //private GameObject[] tiles;
 
     //private float speed = 5f;
     private bool spawned;
 
+    GameManager gameManager;
     void Start()
     {
-        tiles = Resources.LoadAll<GameObject>("Tiles");
+        //tiles = Resources.LoadAll<GameObject>("Tiles");
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     void Update()
     {
         transform.Translate(-GameManager.scenarySpeed * Time.deltaTime, 0f, 0f);
 
-        if (!spawned && transform.position.x < 10f) SpawnTile();
+        if (!spawned && transform.position.x < 5f) SpawnTile();
 
-        if (transform.position.x < -20f) Destroy(gameObject);
+        if (transform.position.x < -20f) gameObject.SetActive(false);
     }
 
     void SpawnTile()
     {
         Vector3 spawnPosition = new Vector3(transform.position.x + 14f, 0f, 0f);
-        Debug.Log(tiles.Length);
-        int tileNumber = Random.Range(0, tiles.Length);
+        //Debug.Log(tiles.Length);
+        //int tileNumber = Random.Range(0, tiles.Length);
 
-        Instantiate(tiles[tileNumber], spawnPosition, Quaternion.identity);
+        //Instantiate(tiles[tileNumber], spawnPosition, Quaternion.identity);
+        gameManager.ActivateRandomTile(spawnPosition);
         spawned = true;
     }
 }
